38a889e072573c4a9f57ac7f9922573d5caca55a
Migawi Mazdon <121274216+migawi@users.noreply.github.com>
2022-12-26 16:03:35 +0000

    Update settings.py

--- studysite/settings.py
+++ studysite/settings.py
@@ -87,13 +87,9 @@ WSGI_APPLICATION = "studysite.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "bioinformatics",
-        "USER": "bioinformatics",
-        "PASSWORD": "Vincent@wanzala.com",
-        "HOST": "localhost",
-        "PORT": "",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
